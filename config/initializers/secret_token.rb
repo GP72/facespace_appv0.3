bfe481897a42d4f1fbabf42cb4127bbe458372a5
Graeme Phelan <greyphelan@gmail.com>
2012-01-01 18:54:30 +0000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacespaceAppv03::Application.config.secret_token = '04115d7ad1b26529f954ab9d0e65dcc5153cab396ce150913c8d85c4ce2cedc6ca6d07fc88b84bb9f91650c7b0ae0439e16c2284cedb2d38a8a5bbbcf0d606c4'
